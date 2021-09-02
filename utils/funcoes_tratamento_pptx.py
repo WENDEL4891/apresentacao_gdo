3500b92cc4d35565fb57fac540f9706c09b1fbfe
wendel4891 <wendel4891@gmail.com>
2021-09-01 21:55:12 -0300

    Notebook pptx-testes fazendo a substituição de todos os cabeçalhos das tabelas, com o mês e ano selecionado. O tratamento está sendo feito nas funções no módulo utils.funcoes_tratamento_pptx.

---
+++ utils/funcoes_tratamento_pptx.py
@@ -0,0 +1,45 @@
+from pptx import Presentation
+
+
+def replace_text(path_name_source_file, path_name_result_file, replace_dict):
+    prs = Presentation(path_name_source_file)
+    for slide in prs.slides:
+        for shape in slide.shapes:
+            if not shape.has_text_frame:
+                continue
+            for match, replacement in replace_dict.items():                
+                if match in shape.text:
+                    for paragraph in shape.text_frame.paragraphs:
+                        for run in paragraph.runs:
+                            p = paragraph._p  # the lxml element containing the `<a:p>` paragraph element
+        #                            remove all but the first run
+                            for idx, run in enumerate(paragraph.runs):
+                                if idx == 0:
+                                    continue
+                                p.remove(run._r)
+                            cur_text = run.text
+                            new_text = cur_text.replace(str(match), str(replacement))
+                            run.text = new_text
+    prs.save(path_name_result_file)
+
+
+def replace_text_table(path_name_source_file, path_name_result_file, replace_dict):
+    prs = Presentation(path_name_source_file)
+    for slide in prs.slides:
+        for shape in slide.shapes:
+                for match, replacement in replace_dict.items():
+                    if shape.has_table:
+                        for cell in shape.table.iter_cells():                            
+                            if match == cell.text:
+                                for paragraph in cell.text_frame.paragraphs:
+                                   for run in paragraph.runs:
+                                       p = paragraph._p  # the lxml element containing the `<a:p>` paragraph element
+        #                            remove all but the first run
+                                       for idx, run in enumerate(paragraph.runs):
+                                           if idx == 0:
+                                               continue
+                                           p.remove(run._r)
+                                       cur_text = run.text
+                                       new_text = cur_text.replace(str(match), str(replacement))
+                                       run.text = new_text
+    prs.save(path_name_result_file)
