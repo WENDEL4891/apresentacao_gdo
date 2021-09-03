7dc2d21cb9f5dbbdee531a971511d7c68ed099c1
wendel4891 <wendel4891@gmail.com>
2021-09-02 23:46:31 -0300

    Adicionando funcionalidade de buscar e acessar shapes pelo shape_id.

--- utils/funcoes_tratamento_pptx.py
+++ utils/funcoes_tratamento_pptx.py
@@ -23,23 +23,26 @@ def replace_text(path_name_source_file, path_name_result_file, replace_dict):
     prs.save(path_name_result_file)
 
 
-def replace_text_table(path_name_source_file, path_name_result_file, replace_dict):
+def replace_text_table_header(path_name_source_file, path_name_result_file, replace_dict):
     prs = Presentation(path_name_source_file)
     for slide in prs.slides:
         for shape in slide.shapes:
                 for match, replacement in replace_dict.items():
                     if shape.has_table:
-                        for cell in shape.table.iter_cells():                            
-                            if match == cell.text:
-                                for paragraph in cell.text_frame.paragraphs:
-                                   for run in paragraph.runs:
-                                       p = paragraph._p  # the lxml element containing the `<a:p>` paragraph element
-        #                            remove all but the first run
-                                       for idx, run in enumerate(paragraph.runs):
-                                           if idx == 0:
-                                               continue
-                                           p.remove(run._r)
-                                       cur_text = run.text
-                                       new_text = cur_text.replace(str(match), str(replacement))
-                                       run.text = new_text
-    prs.save(path_name_result_file)
+                        print(dir(shape.table.rows))
+#                         for cell in shape.table.iter_cells():
+#                         for cell in shape.table.rows:
+#                             print(cell.text)
+#                             if match == cell.text:
+#                                 for paragraph in cell.text_frame.paragraphs:
+#                                    for run in paragraph.runs:
+#                                        p = paragraph._p  # the lxml element containing the `<a:p>` paragraph element
+#         #                            remove all but the first run
+#                                        for idx, run in enumerate(paragraph.runs):
+#                                            if idx == 0:
+#                                                continue
+#                                            p.remove(run._r)
+#                                        cur_text = run.text
+#                                        new_text = cur_text.replace(str(match), str(replacement))
+#                                        run.text = new_text
+#     prs.save(path_name_result_file)
